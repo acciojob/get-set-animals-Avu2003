4bffd7addfd1fb8578d26568071d2c70d16fbf92
Avu2003 <ajayvermaji3000@gmail.com>
2024-11-29 20:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-//complete this code
-class Animal {}
+// Define the Animal class
+class Animal {
+  constructor(species) {
+    this.species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  set species(value) {
+    this._species = value;
+  }
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
+  makeSound() {
+    console.log(The ${this.species} makes a sound);
+  }
+}
+
+// Define the Cat class that extends Animal
+class Cat extends Animal {
+  constructor(species) {
+    super(species);  // Call the Animal constructor
+  }
+
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Define the Dog class that extends Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species);  // Call the Animal constructor
+  }
+
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Expose the classes for testing in Cypress
 window.Cat = Cat;
+window.Dog = Dog;
+window.Animal = Animal;
